c8cfecce7a2ecb5e052d6cb31778c5f48d30c674
Igor Fernandes <igorfercontato@gmail.com>
2021-01-09 23:38:57 -0300

    Project created

---
+++ backend/src/main/java/com/igorfersantos/dsdeliver/DsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.igorfersantos.dsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliverApplication.class, args);
+	}
+
+}
