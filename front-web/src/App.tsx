f4ca42cdc80bbbdc062293e84f4dbbb20b274b4a
Igor Fernandes <igorfercontato@gmail.com>
2021-01-10 17:39:27 -0300

    Add routes adn StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css';
-import Home from './Home';
 import Navbar from './Navbar';
+import Routes from './Routes';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+      <Routes />
   );
 }
 
---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,12 @@
+import StepsHeader from './StepsHeaders'
+import './styles.css'
+
+function Orders() {
+    return(
+        <div className="orders-container">
+            <StepsHeader />
+        </div>
+    )
+}
+
+export default Orders;
